95a17d5a33dfe40490de65359fb5bf235ed4a66f
priyankasrivastava <priyanka.srivastava@raremile.com>
2015-06-25 14:28:41 +0530

    Fixing Conflicts

--- src/com/raremile/training/priyanka/GitApp.java
+++ src/com/raremile/training/priyanka/GitApp.java
@@ -6,9 +6,7 @@ public class GitApp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-System.out.println("My name is"+name+".I know a lot more Git.I know how to merge!. Changed for Part 2.");
-
-System.out.println("My name is"+name+".I Know a lot more git.I know how to merge!. Changed for Part 3.");
+         System.out.println("My name is"+name+".I Know a lot more git.I know how to merge!. Changed for Part 3.");
 
 	}
 
