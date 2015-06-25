d042e8290e19fd388d001870e4c522899b277f00
priyankasrivastava <priyanka.srivastava@raremile.com>
2015-06-25 13:12:57 +0530

    First Commit

---
+++ src/com/raremile/training/priyanka/GitApp.java
@@ -0,0 +1,11 @@
+package com.raremile.training.priyanka;
+
+public class GitApp {
+	private static String name="PriyankaSrivastava";
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("My name is"+name+".I am learning git");
+	}
+
+}
